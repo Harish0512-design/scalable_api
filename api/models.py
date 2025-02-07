d46e7dbfbedd25247631369245cc57f066e30b36
Harish <harishsomsole@gmail.com>
2025-02-07 12:28:44 +0530

    Developed Post, Comment & Like models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField("Tag", blank=True)
+    views = models.PositiveIntegerField(default=0)  # to track post views
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)  # soft delete Flag
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)  # auto generate slug from title
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    # tree for thread reply of comments
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    likes = models.ManyToManyField(User, related_name='liked_comments', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)  # soft delete
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.post.title}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
